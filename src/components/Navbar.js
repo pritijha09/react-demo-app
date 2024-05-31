97a1f154bad3dce29c0119224a1149272c619f60
Priti Kumari <Priti_Kumari@us.crawco.com>
2024-05-31 07:21:35 -0400

    bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="/">Insight Daily
+    Insight Daily
       {/* {props.title} */}
-      </a>
+      
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -18,7 +18,7 @@ export default function Navbar(props) {
             <Link className="nav-link active" to="/">Home</Link>
           </li> */}    
           <li className="nav-item">
-            <Link className="nav-link" to="/general">General</Link>
+            <Link className="nav-link" to="/">General</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/business">Business</Link>
